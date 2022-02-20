6815cf93d3d7649fb1b8423fc796ae9a6e0a847a
wucy10 <wuchuyue2007@gmail.com>
2022-02-20 23:48:14 +0000

    complete level 1

--- install-kaggle-token.sh
+++ install-kaggle-token.sh
@@ -1,8 +1,10 @@
 
-read -p "Please enter your Kaggle API token here (e.g. {\"username\":\"corise\",\"key\":\"3536356fgdf\"}): " api_token
+read -p "Please enter your Kaggle API token here (e.g. {\"username\":\"chuyue\",\"key\":\"1348f710bcf8b3d40009a6a3909cc9b2\"}): " api_token
 echo "Your Kaggle API token is:" $api_token
 echo "Installing Kaggle API token to /home/gitpod/.kaggle/kaggle.json (soft-linked to /workspace/kaggle) ..."
 echo $api_token > /home/gitpod/.kaggle/kaggle.json
 echo "Successfully installed."
 
 chmod 600 /home/gitpod/.kaggle/kaggle.json
+
+
